76bedd9914657303fb3cb105026892af2e648e5c
Erik Grubbs <erikgrubbs@gmail.com>
2019-11-23 13:02:35 -0800

    [prog] Demo work

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.get('/api/pokemon/:id/type', function (req, res) {
 
 
 
-app.listen(3000, function () {
-  console.log('listening on port 3000!');
+app.listen(3001, function () {
+  console.log('listening on port 3001!');
 });
 
